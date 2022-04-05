5c30bf6b7607a628eb987cb8ebd08ef2d0cf607e
Habibullah Saleem <62326123+ItsRaijin@users.noreply.github.com>
2022-04-06 01:19:40 +1000

    base scriptable objects

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : ScriptableObject
+{
+    // Start is called before the first frame update
+    
+    
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    // Start is called before the first frame update
+    [SerializeField] private List<Item> Inv;
+    
+    
+}
