1d5a0c6b6f07ea4d5e596ef673aa7cbf238c0949
christa-zhang-uts <13926086@student.uts.edu.au>
2022-04-07 11:01:42 +1000

    FOV stuff
    so the ai can See

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -7,6 +7,7 @@ public class EnemyFOV : MonoBehaviour
     public float viewRadius;[Range(0, 360)] //how far forward the fov is
     public float viewAngle; //how wide the fov is
 
+    public List<Transform> visibleTargets = new List<Transform>();
     public Vector3 DirFromAngle(float angle)
     {
         return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
@@ -21,5 +22,24 @@ public class EnemyFOV : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FindTargets()
+    {
+        visibleTargets.Clear();
+        Collider2D[] targetsInRadius = Physics2D.OverlapBoxAll(transform.position, new Vector2(viewAngle, 0), viewRadius);
+        for(int i = 0; i < targetsInRadius.Length; i++)
+        {
+            Transform target = targetsInRadius[i].transform;
+            Vector3 targetDir = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, targetDir) < viewAngle / 2)
+            {
+                float targetDist = Vector2.Distance(transform.position, target.position);
+                if(!Physics2D.Raycast(transform.position, targetDir, targetDist))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
     }
 }
