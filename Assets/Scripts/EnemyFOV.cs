8438799f5ef165d97ed00ec108d092c72fbf9fd5
christa-zhang-uts <13926086@student.uts.edu.au>
2022-04-13 18:39:04 +1000

    some minor stuff

--- Assets/Scripts/EnemyFOV.cs
+++ Assets/Scripts/EnemyFOV.cs
@@ -6,15 +6,15 @@ public class EnemyFOV : MonoBehaviour
 {
     public float viewRadius;[Range(0, 360)] //how far forward the fov is
     public float viewAngle; //how wide the fov is
-
-<<<<<<< Updated upstream
-=======
     public float meshResolution;
 
->>>>>>> Stashed changes
     public List<Transform> visibleTargets = new List<Transform>();
-    public Vector3 DirFromAngle(float angle)
+    public Vector3 DirFromAngle(float angle, bool isGlobalAngle)
     {
+        if (!isGlobalAngle)
+        {
+            angle += transform.eulerAngles.y;
+        }
         return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
     }
     // Start is called before the first frame update
@@ -47,22 +47,47 @@ public class EnemyFOV : MonoBehaviour
             }
         }
     }
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-
     void DrawFOV()
     {
-        int sections = Mathf.RoundToInt(viewAngle * meshResolution);
+        int sections = Mathf.RoundToInt(viewAngle * meshResolution); //number of rays the fov cone will use
         float sectionAngleSize = viewAngle / sections;
-
+        List<Vector2> viewpoints = new List<Vector2>();
+        for(int i = 0; i <= sections; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + sectionAngleSize * i;
+            
+        }
     }
->>>>>>> Stashed changes
-=======
 
-    void DrawFOV()
+    ViewCastInfo ViewCast(float globalAngle)
     {
+        Vector2 dir = DirFromAngle(globalAngle, true);
+        RaycastHit2D hit;
 
+        if(Physics2D.Raycast(transform.position, dir, viewRadius))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, hit.point, hit.distance, globalAngle);
+        }
     }
->>>>>>> Stashed changes
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector2 point;
+        public float distance;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector2 _point, float _distance, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            distance = _distance;
+            angle = _angle;
+
+        }
+    }
 }
