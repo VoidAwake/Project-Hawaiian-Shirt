386ca2a1d34861fb42a60142f35601440948ec47
Francisco Castro <francisco.castro3030@gmail.com>
2022-04-20 03:26:39 +1000

    updated melee attack and started projectile attacks

--- Assets/Scripts/Hawaiian/Unit/UnitPlayer.cs
+++ Assets/Scripts/Hawaiian/Unit/UnitPlayer.cs
@@ -1,3 +1,4 @@
+using Hawaiian.Input;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,30 +6,128 @@ namespace Hawaiian.Unit
 {
     public class UnitPlayer : Unit, IUnit
     {
-        
         [SerializeField] private GameObject _damageReference;
-        [SerializeField] private GameObject firePoint;
+        [SerializeField] private float _offset;
+        [SerializeField] private float _damageIndicatorOffset;
+        [SerializeField] private Transform _firePoint;
 
 
-        private bool _attackFlag = false;
+        private PlayerAction action;
+        private PlayerInput _input;
+        private UnitAnimator _animator;
+        private Vector3 _lastAttackPosition;
+        private Vector2 _rotation;
+        private bool _attackFlag;
+        private bool isLookingLeft;
+        private bool _isJoystickNeutral = true;
 
+        #region RangedAttackVariables
+
+        [SerializeField] private float minOffset;
+        [SerializeField] private float maxOffset;
+        
+        #endregion
+        
+        
+        private void Awake()
+        {
+            action = new PlayerAction();
+            _input = GetComponent<PlayerInput>();
+            _animator = GetComponent<UnitAnimator>();
+            
+            action.Player.Rotate.performed += ctx => UpdateRotation(ctx.ReadValue<Vector2>());
+            action.Player.Rotate.canceled += ctx => CancelRotation();
+
+        }
+
+        void UpdateRotation(Vector2 newValue)
+        {
+            _rotation = newValue;
+            _isJoystickNeutral = false;
+        }
+
+        void CancelRotation()
+        {
+            _rotation = Vector2.zero;
+            _isJoystickNeutral = true;
+        }
+
+        private void OnEnable()
+        {
+            action.Enable();
+            _rotation = Vector2.zero;
+        }
+
+        private void OnDisable()
+        {
+            
+            action.Disable();
+        }
+
+        
         public void OnMove(InputValue value)
         {
             move = value.Get<Vector2>();
-        } // Debug.Log("Stick: " + value.Get<Vector2>()); }
+        } 
+        
         public void OnActionA(InputValue value) { isRunning = value.Get<float>() > 0.1f;} //Debug.Log("Button: " + value.Get<float>()); }
 
 
         public void OnAttack(InputValue value)
         {
-         //   GameObject indicator = Instantiate(_damageReference, firePoint.transform.position, Quaternion.Euler(0,0,-90));
-            GameObject indicator = Instantiate(_damageReference, firePoint.transform);
+            #region Ranged Attack
+            
+            
+            
+            
+            
 
-            indicator.GetComponent<DamageIndicator>().Initialise(5,_attackFlag,this);
-            _attackFlag = !_attackFlag;
+            #endregion
+            
+            
+            #region MeleeAttack
+           //  Vector3 playerInput;
+           //  float angle;
+           //  
+           //  var position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+           // // var mouse = UnityEngine.Input.mousePosition;
+           //  
+           //  if (_input.currentControlScheme == "Gamepad")
+           //  {
+           //
+           //      if (_isJoystickNeutral)
+           //          _rotation = _animator.IsLookingLeft ? Vector2.left : Vector2.right;
+           //      
+           //      playerInput = _rotation;
+           //      _lastAttackPosition = position + (Vector3)_rotation * _offset;
+           //      angle = Mathf.Atan2(_rotation.y, _rotation.x) * Mathf.Rad2Deg;
+           //
+           //  }
+           //  else
+           //  {
+           //      playerInput = UnityEngine.Input.mousePosition;
+           //      Vector3 worldPosition = Camera.main.ScreenToWorldPoint(playerInput);
+           //      worldPosition.z = 0f; // set to zero since its a 2d game
+           //      var mouseDirection = (worldPosition - position).normalized;
+           //      _lastAttackPosition = position + mouseDirection * _offset;
+           //      Vector3 difference = Camera.main.ScreenToWorldPoint(playerInput) -position;
+           //      difference.Normalize();
+           //      angle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+           //  }
+           //  
+           //  
+           //  _firePoint.position = _lastAttackPosition;
+           //  GameObject indicator = Instantiate(_damageReference,_lastAttackPosition,Quaternion.Euler(new Vector3(0,0,angle + _damageIndicatorOffset )),_firePoint);
+           //
+           //  indicator.GetComponent<DamageIndicator>().Initialise(5,_attackFlag,this);
+           //  _attackFlag = !_attackFlag;
+            #endregion
+
         }
-    
 
+        public Vector3 GetPosition() =>  transform.position;
+        
+        
         public void TakeDamage(float damage)
         {
             _health -= damage;
