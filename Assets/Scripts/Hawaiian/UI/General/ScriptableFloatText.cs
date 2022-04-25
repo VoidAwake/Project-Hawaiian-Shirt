0826e0ad778c1d4fd31183acba8228c5164fa3a1
Jack Coggins <voidawake90@gmail.com>
2022-04-25 20:44:52 +1000

    ScriptableFloat UI

---
+++ Assets/Scripts/Hawaiian/UI/General/ScriptableFloatText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Hawaiian.Utilities;
+using TMPro;
+using UI.Core;
+using UnityEngine;
+
+namespace Hawaiian.UI.General
+{
+    public class ScriptableFloatText : DialogueComponent<Dialogue>
+    {
+        [SerializeField] private TMP_Text text;
+        [SerializeField] private ScriptableFloat scriptableFloat;
+        [SerializeField] private string format;
+
+        private void OnValueChanged()
+        {
+            text.text = String.Format(format, scriptableFloat.Value);
+        }
+
+        protected override void Subscribe()
+        {
+            scriptableFloat.valueChanged.AddListener(OnValueChanged);
+
+            try
+            {
+                OnValueChanged();
+            }
+            catch (FormatException)
+            {
+                enabled = false;
+                
+                throw;
+            }
+        }
+
+        protected override void Unsubscribe()
+        {
+            scriptableFloat.valueChanged.RemoveListener(OnValueChanged);
+        }
+    }
+}
