2ac52452e9097984847f38e0ddfa82e3524f39fb
Jack Coggins <voidawake90@gmail.com>
2022-05-09 15:03:17 +1000

    Create simultaneous input tester
    Used to verify the duplication bug

---
+++ Assets/Scripts/Hawaiian/Inventory/SimultaneousInputTester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hawaiian.Inventory
+{
+    public class SimultaneousInputTester : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> gameObjects;
+        [SerializeField] private string methodName;
+
+        [ContextMenu("SendMessages")]
+        private void SendMessages()
+        {
+            foreach (var gameObject in gameObjects)
+            {
+                gameObject.BroadcastMessage(methodName);
+            }
+        }
+    }
+}
