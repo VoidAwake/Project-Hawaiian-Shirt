9b0fcbc65a20f798e6a430cfeab3000f7123d132
Eddie <64623283+leaire@users.noreply.github.com>
2022-05-10 20:21:45 +1000

    Customisation and lobby progress

---
+++ Assets/Scripts/Hawaiian/UI/CharacterSelect/LobbyWindow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Hawaiian.Unit;
+
+namespace Hawaiian.UI.CharacterSelect
+{
+    public class LobbyWindow : MonoBehaviour
+    {
+        [SerializeField] TextMeshPro textNumber;
+        [SerializeField] TextMeshPro textPrompt;
+        [SerializeField] TextMeshPro textReady;
+        [SerializeField] Unit.UnitAnimator unit;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
