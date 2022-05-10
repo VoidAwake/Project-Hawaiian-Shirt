86102d697cc7d667614756099cd535795a62a943
Jack Coggins <voidawake90@gmail.com>
2022-05-10 14:08:40 +1000

    Convert controls instructions to dialogue

---
+++ Assets/Scripts/Hawaiian/UI/ControlsInstructions/ControlsInstructionsDialogue.cs
@@ -0,0 +1,13 @@
+﻿using UI.Core;
+
+namespace Hawaiian.UI.ControlsInstructions
+{
+    public class ControlsInstructionsDialogue : Dialogue
+    {
+        protected override void OnClose() { }
+
+        protected override void OnPromote() { }
+
+        protected override void OnDemote() { }
+    }
+}
---
+++ Assets/Scripts/Hawaiian/UI/General/OpenDialogueButton.cs
@@ -0,0 +1,17 @@
+﻿using UI.Core;
+using UnityEngine;
+
+namespace Hawaiian.UI.General
+{
+    public class OpenDialogueButton : Button<Dialogue>
+    {
+        [SerializeField] private GameObject dialoguePrefab;
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            
+            Instantiate(dialoguePrefab, dialogue.transform.parent);
+        }
+    }
+}
