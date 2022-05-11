d9da65b8368865e1ddc0fcff71a38982a1ddb1ee
Francisco Castro <francisco.castro3030@gmail.com>
2022-05-11 18:22:16 +1000

    Updated Camera to zoom in and out depending on the current players position

--- Assets/Scripts/Hawaiian/Camera/CameraController.cs
+++ Assets/Scripts/Hawaiian/Camera/CameraController.cs
@@ -10,7 +10,13 @@ public class CameraController : MonoBehaviour
    [SerializeField] private Camera _camera;
    [SerializeField] private float _smoothTime;
    [SerializeField]private Vector2 _offset;
+   
+   [Header("Zoom Settings")]
+   [SerializeField] private float _maxZoom;
+   [SerializeField] private float _minZoom;
+   [SerializeField] private float _maximumDistance;
 
+   
    private List<Transform> players = new List<Transform>();
    private Vector2 _center;
    private Vector2 velocity;
@@ -33,27 +39,44 @@ public class CameraController : MonoBehaviour
    private void FixedUpdate()
    {
       _center = GetCenterPoint(players.ToArray());
+      
       var newPosition = _center + _offset;
       transform.position = Vector2.SmoothDamp(transform.position, newPosition, ref velocity, _smoothTime);
+
+      var zoom = Mathf.Lerp(_maxZoom, _minZoom, GetGreatestDistance() / _maximumDistance);
+      _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, zoom, Time.deltaTime);
    }
 
-   public Vector2 GetCenterPoint(Transform[] players)
+   public Bounds EncapsulateTargets()
    {
-      
-      if (players.Length <= 0)
-         return Vector2.zero;
-
-      if (players.Length == 1)
-         return players[0].position;
-
-      //creating bounds of the first player
       var bounds = new Bounds(players[0].position, Vector3.zero);
       
       //adding additional bounds to additonal players
       foreach (Transform player in players)
          bounds.Encapsulate(player.position);
 
+      return bounds;
+   }
+
+   private  Vector2 GetCenterPoint(Transform[] players)
+   {
+      if (players.Length <= 0)
+         return Vector2.zero;
+
+      if (players.Length == 1)
+         return players[0].position;
+
+      var bounds = EncapsulateTargets();
+
       return bounds.center;
    }
+
+   private float GetGreatestDistance()
+   {
+      var bounds = EncapsulateTargets();
+      return bounds.size.x > bounds.size.y ? bounds.size.x : bounds.size.y;
+   }
+   
+   
    
 }
