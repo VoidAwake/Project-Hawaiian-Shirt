72a0e7db906f38e6f4ab5b5723d2deadfc325fad
christa-zhang-uts <13926086@student.uts.edu.au>
2022-05-26 00:37:07 +1000

    uhhuhuhhhh
    sound managers started i'll deal with it later

--- Assets/Scripts/Hawaiian/Utilities/AudioManager.cs
+++ Assets/Scripts/Hawaiian/Utilities/AudioManager.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
+    AudioClip[] clips;
+    
     // Start is called before the first frame update
     void Start()
     {
