644632e94d85a64b6e0798f26a7c3f632cf6b427
VoidAwake <voidawake90@gmail.com>
2022-07-12 17:43:06 +1000

    Add comment

--- Assets/Scripts/Hawaiian/Game/GameModeSO.cs
+++ Assets/Scripts/Hawaiian/Game/GameModeSO.cs
@@ -6,6 +6,7 @@ namespace Hawaiian.Game
     [CreateAssetMenu(order = 0, menuName = "Hawaiian/GameMode")]
     public class GameModeSO : ScriptableObject
     {
+        // TODO: Ideally, we should be able to remove this enum.
         public GameMode gameMode;
         public string displayName;
         public string description;
