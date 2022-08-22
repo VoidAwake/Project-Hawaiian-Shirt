88757624852166198c5b4d3a7847396b9807e021
Habib <habibleton@gmail.com>
2022-08-23 02:32:10 +1000

    TRAPS ARE FINALLY DOPNE BITCHES
    A WEEK OVERDUE BUT LETS FUCKING GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- Assets/Scripts/Hawaiian/Traps/CascadeTrap.cs
+++ Assets/Scripts/Hawaiian/Traps/CascadeTrap.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CascadeTrap : MonoBehaviour
+public class CascadeTrap : TrapController
 {
     [SerializeField] private float intervalTime;
     // Start is called before the first frame update
-    void Start()
+    [SerializeField] TrapController[] kiddies;
+    private float timer = 0.0f;
+    public float timeBetweenTrigger;
+    public int pos = 0;
+    public bool loop;
+    
+    public override void TriggerTrap()
     {
-        
+        triggered = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            if (kiddies[pos] != null)
+            {
+                kiddies[pos].TriggerTrap();
+                pos += 1;
+            }
+
+            if (pos > kiddies.Length - 1)
+            {
+                pos = 0;
+                if (!loop)
+                {
+                    triggered = false;
+                }
+            }
+        }
     }
 }
