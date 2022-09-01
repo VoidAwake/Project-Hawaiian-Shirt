a007354f54eb528e0ad798ff48ac72259b5816e8
VoidAwake <voidawake90@gmail.com>
2022-09-01 16:33:00 +1000

    Add tutorial game mode

---
+++ Assets/Scripts/Hawaiian/Game/GameModes/Tutorial/TutorialModeManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Hawaiian.Game.GameModes.Tutorial
+{
+    [CreateAssetMenu(order = 0, menuName = "Hawaiian/Managers/GameModeManager/TutorialModeManager")]
+    public class TutorialModeManager : ModeManager<TutorialSceneReference>
+    {
+        
+    }
+}
